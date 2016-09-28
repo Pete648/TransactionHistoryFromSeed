e0282a3c8a98de017cb323ef6752efe82d3b2e50
twood <twood@TOMWOOD2>
2016-09-28 15:53:02 -0700

    Fixed up detail table

--- TransactionHistoryFromSeed/src/client/app/message-wrapper.ts
+++ TransactionHistoryFromSeed/src/client/app/message-wrapper.ts
@@ -1,6 +1,22 @@
-﻿
+﻿//import { Pipe, PipeTransform } from '@angular/core';
+//import { DecimalPipe } from '@angular/common';
+//import { JhaDateStringPipe } from './jha.pipe';
+//import { JhaMoneyPipe } from './jha.pipe';
+
+enum DataType {
+	Text = 0,
+	Date = 1,
+	Money = 2
+}
+
+enum ClassName {
+	Left,
+	LeftBold,
+	RightBold
+}
 class DetailColumn {
-    constructor(public description: string, public value: any) {
+    constructor(public value: any, public className: string, public dataType: DataType) {
+		// format is undefined if not specified
     }
 }
 
@@ -11,14 +27,69 @@ class DetailRow {
 export class MessageWrapper {
 	constructor(public showDetail: boolean, public symMessage: any) {
 
-        let detailRow = new DetailRow();
-        detailRow.detailColumns.push(new DetailColumn("Post Date", this.symMessage.PostDate));
-        detailRow.detailColumns.push(new DetailColumn("Eff Date", this.symMessage.EffDate));
-        detailRow.detailColumns.push(new DetailColumn("Fees", this.symMessage.Fee));
+		//let date = new JhaDateStringPipe();	// make static
+		//let money = new JhaMoneyPipe();
+		//let decimal = new DecimalPipe();
 
-        this.detailRows.push(detailRow);
-    }
+		this.addRow();
 
-    detailRows: DetailRow[] = [];
+		this.addColumn(this.symMessage.Tran, ClassName.LeftBold);
+
+		this.addRow();
+
+		this.addColumn(this.symMessage.IDType == "0" ? "Share ID:" : "Loan ID", ClassName.Left);
+		this.addColumn(this.symMessage.ID, ClassName.RightBold);
+
+		this.addColumn("Sequence:", ClassName.Left);
+		this.addColumn(this.symMessage.Sequence, ClassName.RightBold);
+
+		this.addColumn("Branch:", ClassName.Left);
+		this.addColumn(this.symMessage["Branch"], ClassName.RightBold);
+
+		this.addRow();
+
+		this.addColumn("Effective Date:", ClassName.Left);
+		this.addColumn(this.symMessage.EffDate, ClassName.RightBold, DataType.Date);
+
+		this.addColumn("User Number:", ClassName.Left);
+		this.addColumn(this.symMessage.UserNum, ClassName.RightBold);
+
+		this.addColumn("Void:", ClassName.Left);
+		this.addColumn(this.symMessage.Voided == "1" ? "Yes" : "No", ClassName.RightBold);
+
+		this.addRow();
+
+		this.addColumn("Post Date:", ClassName.Left);
+		this.addColumn(this.symMessage.PostDate, ClassName.RightBold, DataType.Date);
+
+		this.addColumn("User Override:", ClassName.Left);
+		this.addColumn(this.symMessage.Override, ClassName.RightBold);
+
+		this.addColumn("Trans Amt:", ClassName.Left);
+		this.addColumn(this.symMessage.TranAmount, ClassName.RightBold, DataType.Money);
+	}
+
+	addRow(): void { this.detailRows.push(new DetailRow()); }
+
+	addColumn(value: string, className: ClassName, dataType?: DataType): void {
+
+		if (dataType == null) {
+			dataType = DataType.Text;
+		}
+
+		let classNameString: string = "jha-left";
+
+		if (className === ClassName.LeftBold) {
+			classNameString = "jha-left-bold";
+			
+		}
+		else if (className === ClassName.RightBold) {
+			classNameString = "jha-right-bold";
+		}
+
+		this.detailRows[this.detailRows.length - 1].detailColumns.push(new DetailColumn(value, classNameString, dataType));
+	}
+
+	detailRows: DetailRow[] = [];
 }
 
